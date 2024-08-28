b0dac6bb372b1582693daad51d149faf30fb8be2
Ruben <ruben.s03@mail.ru>
2024-08-28 13:01:36 +0300

    add pyinterface for a1

---
+++ third_party/unitree_legged_sdk/a1/build/CMakeFiles/robot_interface_a1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_interface_a1.dir/python_interface.cpp.o"
+  "CMakeFiles/robot_interface_a1.dir/python_interface.cpp.o.d"
+  "robot_interface_a1.cpython-310-x86_64-linux-gnu.so"
+  "robot_interface_a1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_interface_a1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
